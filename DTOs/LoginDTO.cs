75bc9868955bcce6b8c19e0ec0024efa095bbbf0
Cosmin-Ioan Grigoruta <grigorutacosmin@gmail.com>
2022-01-27 09:26:54 +0200

    Implement login/register

---
+++ DTOs/LoginDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MCExercise.DTOs
+{
+    public class LoginDTO
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
