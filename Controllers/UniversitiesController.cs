bbe5c79cb3c59d913a8f4d7b4c54f396445dc525
Cosmin-Ioan Grigoruta <grigorutacosmin@gmail.com>
2022-01-30 22:20:38 +0200

    Impelement University Controller

---
+++ Controllers/UniversitiesController.cs
@@ -0,0 +1,40 @@
+﻿using MCExercise.Models.DTOs;
+using MCExercise.Services.UniversityService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MCExercise.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UniversitiesController : ControllerBase
+    {
+        private readonly IUniversityService _universityService;
+
+        public UniversitiesController(IUniversityService universityService)
+        {
+            _universityService = universityService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(UniversityRegisterDTO universityRegisterDTO)
+        {
+            var response = await _universityService.Register(universityRegisterDTO);
+            if (response == null)
+            {
+                return BadRequest(new { Message = "Email already used!" });
+            }
+            return Ok(response);
+        }
+        [HttpPost("authenticate")]
+        public async Task<IActionResult> Authenticate(UniversityRequestDTO universityRequestDTO)
+        {
+            var response = await _universityService.Authenticate(universityRequestDTO);
+            if (response == null)
+            {
+                return BadRequest(new { Message = "Email or Password is invalid!" });
+            }
+            return Ok(response);
+        }
+    }
+}
