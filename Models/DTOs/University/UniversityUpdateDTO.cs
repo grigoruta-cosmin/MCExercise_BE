cd6372811992f0d81a79d254a2f92e3d586bdcca
Cosmin-Ioan Grigoruta <grigorutacosmin@gmail.com>
2022-02-01 01:29:01 +0200

    Change DTO folder structure

---
+++ Models/DTOs/University/UniversityUpdateDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace MCExercise.Models.DTOs
+{
+    public class UniversityUpdateDTO
+    {
+        public Guid UniversityId { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string Type { get; set; }
+        public string Email { get; set; }
+        public string NewPassword { get; set; }
+        public string About { get; set; }
+    }
+}
